f2b142d27e265e57d92bf331918b721b6d2a6094
dluman <dluman@allegheny.edu>
2026-02-01 22:42:22 -0500

    Update tests to use actual spec

--- tests/portfolio.spec.ts
+++ tests/portfolio.spec.ts
@@ -13,3 +13,15 @@ test('has h1', async ({page}) => {
   await page.goto(process.env.BASE_URL);
   await expect(page.getByRole('heading', { name: 'Astro', level: 1 })).toBeVisible();
 });
+
+test('has nav', async ({page}) => {
+  await page.goto(process.env.BASE_URL);
+  const nav = page.locator('nav');
+  await expect(nav).toBeVisible();
+});
+
+test('has footer', async ({page}) => {
+  await page.goto(process.env.BASE_URL);
+  const footer = page.locator('footer');
+  await expect(footer).toBeVisible();
+});
