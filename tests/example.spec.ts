2a33b367c7cf0e9e23496698e4bb2e4e650547c3
dluman <dluman@allegheny.edu>
2026-02-01 22:24:57 -0500

    Adding a second test, now that the first one works

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,7 +2,10 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto(process.env.BASE_URL);
+  await expect(page).toHaveTitle(/Astro/);
+});
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test('has h1', async ({page}) => {
+  await page.goto(process.env.BASE_URL);
+  await expect(page.getByRole('heading', { name: 'Astro', level: 1 })).toBeVisible();
 });
