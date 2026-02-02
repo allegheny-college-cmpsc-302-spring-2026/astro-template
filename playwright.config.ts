416902717caf3da5259ca8ed583c9f1f9c2a05ac
dluman <dluman@allegheny.edu>
2026-02-01 22:29:50 -0500

    Update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,8 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'],
-    ['json', {outputFile:'./test-results/test-results.json'}]
+    ['json', {outputFile:'./test-results/test-results.json'}],
+    ['html', {open: 'always'}]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
